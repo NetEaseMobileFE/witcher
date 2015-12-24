b13bb768931cdc4d976f045126083bbc9c32b193
re54k <ponyowa@163.com>
2015-12-24 13:51:38 +0800

    test

---
+++ webpack.config.test.js
@@ -0,0 +1,58 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+	devtool: 'eval',
+	entry: {
+		app: './app/js/app.jsx',
+		vendor: [
+			'react', 'react-dom', 'react-router', 'history'
+		]
+	},
+	output: {
+		path: path.join(__dirname, 'dist/js'),
+		filename: 'bundle.js',
+		publicPath: 'http://c.3g.163.com/nc/qa/witcher/js/'
+	},
+	plugins: [
+		new webpack.optimize.CommonsChunkPlugin({
+			name: 'vendor',
+			filename: 'vendor.bundle.js',
+			minChunks: Infinity
+		}),
+		new webpack.optimize.CommonsChunkPlugin({
+			name: 'app',
+			children: true,
+			minChunks: 2
+		}),
+		new webpack.DefinePlugin({
+			'process.env': {
+				'NODE_ENV': JSON.stringify('production')
+			}
+		})
+	],
+	module: {
+		loaders: [
+			{
+				test: /\.css$/,
+				include: path.join(__dirname, 'app/css'),
+				loader: 'style!css!autoprefixer-loader?{browsers:["last 2 version", "Android >= 4.0"]}'
+			}, { // LESS
+				test: /\.less$/,
+				loader: 'style!css!less'
+			}, {
+				test: /\.js[x]?$/,
+				include: path.join(__dirname, 'app/js'),
+				exclude: path.join(__dirname, 'app/js/plugins'),
+				loader: 'babel'
+			}
+		]
+	},
+	resolve: {
+		extensions: ['', '.js', '.jsx'],
+		alias: {
+			js: path.join(__dirname, 'app/js'),
+			css: path.join(__dirname, 'app/css')
+		}
+	}
+};
